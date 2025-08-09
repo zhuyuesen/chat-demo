526a199d9b96a2bfebe4e103f1a2413678a9bfd2
zhuyuesen <907980361@qq.com>
2025-08-09 18:07:35 +0800

    Add main App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatDemo from './components/ChatDemo';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ChatDemo />
+    </div>
+  );
+}
+
+export default App;
